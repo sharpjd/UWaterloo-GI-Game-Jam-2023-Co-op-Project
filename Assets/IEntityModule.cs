405f4aa99cd305c08dde1e54271d18262cf3271e
sharpjd <sharpieinthebox@gmail.com>
2023-02-03 20:42:40 -0500

    Commentated on IEntityModule

--- Assets/IEntityModule.cs
+++ Assets/IEntityModule.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace Assets
 {
+
+    /// <summary>
+    /// Use this for any scripts that need visibility to and from an <see cref="Entity"/>.
+    /// Make sure you add the instance to an Entity's <see cref="Entity.entityModules"/> and 
+    /// set this <see cref="ParentEntity"/> to it
+    /// </summary>
     internal interface IEntityModule
     {
         Entity ParentEntity { get; set; }
