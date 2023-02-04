19f9bc90e74149d730877b89bae0cdd4e7a05424
sharpjd <sharpieinthebox@gmail.com>
2023-02-03 20:19:59 -0500

    Preliminary base classes

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -1,9 +1,12 @@
+using Assets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+
+    List<IEntityModule> entityModules = new();
 
     /*
     Texture2D texture2D;
@@ -21,7 +24,7 @@ public class Entity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
---
+++ Assets/EntityTracker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityTracker : MonoBehaviour
+{
+
+    public static List<Entity> entities = new();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
