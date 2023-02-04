70d00f5e816b74bd03f3cfb7ded12c54a7129230
sharpjd <sharpieinthebox@gmail.com>
2023-02-03 21:38:28 -0500

    Added (probably not yet working) Map Vector positioning thing

---
+++ Assets/Scripts/Exceptions/ComponentNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+class ComponentNotFoundException : Exception {
+    public ComponentNotFoundException(string errorMessage) 
+    { 
+        Debug.Log(errorMessage);
+    }
+}
---
+++ Assets/Scripts/IHittable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    internal interface IHittable
+    {
+
+        float Hitpoints
+        {
+            get; set;
+        }
+
+        void OnDeath()
+        {
+
+        }
+
+        public void OnHit(float damage)
+        {
+            Hitpoints -= damage;
+            if(Hitpoints < 0)
+            {
+                OnDeath();
+            }
+        }
+
+    }
+}
