fa3a8336f8b9e8525219257ff8bb1608c37f2586
sharpjd <sharpieinthebox@gmail.com>
2023-02-03 20:34:29 -0500

    added Weapon class

--- Assets/ComponentNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-class ComponentNotFoundException : Exception {
-    public ComponentNotFoundException(string errorMessage) 
-    { 
-        Debug.Log(errorMessage);
-    }
-}
