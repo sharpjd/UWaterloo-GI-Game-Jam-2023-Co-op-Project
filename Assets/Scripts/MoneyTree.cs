21d3e3377d746dbaa45269f8039661a63c0a73a0
sheep01 <77167008+sheep01@users.noreply.github.com>
2023-02-04 23:14:02 -0500

    Started MoneyTree

---
+++ Assets/Scripts/MoneyTree.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyTree : TowerEntity
+{
+    // Start is called before the first frame update
+    
+        
+    
+}
