c2f7eb84e4ca65e3cabecb6b8c08b77165e8dbd0
sheep01 <77167008+sheep01@users.noreply.github.com>
2023-02-05 09:46:06 -0500

    Father mother tree

---
+++ Assets/Scripts/FatherTree.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatherTree : TowerEntity
+{
+    [SerializeField]
+    float buffRange;
+
+    [SerializeField]
+    float buffAmount;
+
+    List<TowerEntity> towersInRange;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+
+        towersInRange = GameHandler.instance.entityTracker.GetTowersInArea(transform.position, buffRange);
+
+        foreach (TowerEntity tower in towersInRange)
+        {
+            tower.fireRateSecs /= buffAmount;
+        }
+
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        foreach (TowerEntity tower in towersInRange)
+        {
+            tower.fireRateSecs *= buffAmount;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MotherTree.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherTree : TowerEntity
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    float buffRange;
+
+    [SerializeField]
+    float buffAmount;
+
+    List<TowerEntity> towersInRange;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+
+        towersInRange = GameHandler.instance.entityTracker.GetTowersInArea(transform.position, buffRange);
+
+        foreach (TowerEntity tower in towersInRange)
+        {
+            tower.fireRateSecs /= buffAmount;
+        }
+
+    }
+
+    // Update is called once per frame
+    private void OnDestroy()
+    {
+        foreach (TowerEntity tower in towersInRange)
+        {
+            tower.fireRateSecs *= buffAmount;
+        }
+    }
+
+}
