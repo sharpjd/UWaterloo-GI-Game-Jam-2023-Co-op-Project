3e9efbb1b1b570c3e46292cbb5ad117cd6215d43
sheep01 <77167008+sheep01@users.noreply.github.com>
2023-02-04 21:34:37 -0500

    Made Durian Tree

--- Assets/Scripts/DurianProjectile.cs
+++ Assets/Scripts/DurianProjectile.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DurianTree : TowerEntity
+public class DurianProjectile : StandardProjectileEntity
 {
     // Start is called before the first frame update
-    
-   
+    public override void OnDestruction()
+    {
+        
+    }
 }
